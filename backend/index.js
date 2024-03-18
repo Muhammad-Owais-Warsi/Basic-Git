900cc15606895d0333833467c403e966128d4523
Owais <warsimuhammadowais@gmail.com>
2024-03-18 22:54:19 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"https://basicgit.onrender.com/"
+}));
 app.use(express.json());
 
 
